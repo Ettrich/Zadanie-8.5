8deabd753f03d8e487eea21e0fff01aacd46c5a5
Filip Tchorek <filip.tchorek@gmail.com>
2017-12-30 14:01:49 +0100

    Task 8.5

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var a = prompt ("Enter 'a' value");
+var h = prompt ("Enter 'h' value");
+
+var triangleArea = a * h / 2;
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+
